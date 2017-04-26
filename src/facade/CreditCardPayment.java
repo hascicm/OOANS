0e3f5102ba834b0fa9ab7f67e420820943d9413b
hascic <hascicm.mh@gmail.com>
2017-04-26 21:37:36 +0200

    facade

--- src/facade/CreditCardPayment.java
+++ src/facade/CreditCardPayment.java
@@ -2,4 +2,16 @@ package facade;
 
 public class CreditCardPayment implements PaymentSubsystem{
 
+	@Override
+	public boolean pay(double amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean verify() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
